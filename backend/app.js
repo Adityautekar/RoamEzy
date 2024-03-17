a10b45b146b4151a418263ea7161cec3b9134a36
AdityaUtekar <adityautekar9@gmail.com>
2024-03-17 20:54:15 +0530

    cors22

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,11 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", req.headers.origin);
-//     res.header("Access-Control-Allow-Credentials", true);
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 
 app.use("/", userRouter)
