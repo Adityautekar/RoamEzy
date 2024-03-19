7d1f04730dbea7c5232fdd7db7e89e415dfd9790
AdityaUtekar <adityautekar9@gmail.com>
2024-03-19 20:56:51 +0530

    cookie

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
 
-
+app.use(cookieParser());
 //middleware to handle incoming json data
 app.use(express.json({
     limit:"16kb"
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended:true, limit : "16kb"}));
 app.use(express.static("public"));
 //*app.use('/uploads' , express.static(process.cwd() + '/uploads'));
 
-app.use(cookieParser());
+
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", req.headers.origin);
 //     res.header("Access-Control-Allow-Credentials", true);
